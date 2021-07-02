c2c5b057e4209b8796bc3bd76dfbf5b059985d2e
thanhle2206 <86834042+thanhle2206@users.noreply.github.com>
2021-07-02 19:32:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Vân à!',
+    introDesc: 'Chúc mừng sinh nhật nha!',
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Sinh nhật cui vẻ nhó',
+    desc: 'Nay sinh nhật m rồi có muốn đc tặng quà ko nè😝😝',
+    btnYes: 'Thôi ko cần đâu!😅😅',
+    btnNo: 'Có, muốn chứ!😋😋',
+    question:'Tại sao lại ko cần???',
+    btnReply: 'Gửi đi!',
+    reply: 'T lớn rồi mà ko cần phải quà cáp đâu, m nhớ sinh nhật t là đc r😅😅',
+    mess: 'Biết ngay mà😂😂. T hiểu m lắm😁😁',
+    messDesc: 'M ko cần nhưng mà t vẫn cần quà nha! Nhớ tặng quà cho t đó!',
+    messLink: 'https://www.facebook.com/profile.php?id=100034011608024'
 }
